48c24c7f2fe920fd2a2d9b12dce68790e1bc2b58
novellito <christian.trinidad.501@my.csun.edu>
2018-04-05 15:09:25 -0700

    set up boiler electron app with react

---
+++ project4/readme.md
@@ -0,0 +1,7 @@
+# Project 4 - Facebook Lite
+
+How to get started:
+
+After cloning the repo, run `npm install`
+
+To develop on the application simply type `npm run dev` - this will spin the electron app as well as the browser app (dont worry about the browser)
---
+++ project4/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App';
+import './index.css';
+import 'photonkit/dist/css/photon.css';
+import 'photonkit/fonts/photon-entypo.ttf';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
