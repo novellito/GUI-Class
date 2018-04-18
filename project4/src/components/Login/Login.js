4c6d65da7a18ba4d2f939985ad9a555daf6020c0
George <gnasour@gmail.com>
2018-04-17 20:25:47 -0700

    updated style for textfield and button

--- project4/src/components/Login/Login.js
+++ project4/src/components/Login/Login.js
@@ -1,13 +1,32 @@
 import React, {Component} from 'react';
 import './Login.css'
 import logo from './facebook.png';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 
 
 class Login extends Component {
-    constructor() {
-        super();
-        
+    
+    state = {
+        username:'',
+        password:''
+    }
+
+    handleLogin = (e) => {
+        e.preventDefault();
+        console.log(this.state.username);
+        console.log(this.state.password);
+
+        ///fetch...
+    }
+
+    handleUsername = (username) => {
+        this.setState({username:username},()=>console.log(this.state.username))
+    }
+
+    handlePassword = (password) => {
+        this.setState({password:password}, ()=> console.log(this.state.password))
     }
 
     componentDidMount() {
@@ -19,15 +38,15 @@ class Login extends Component {
            
             <div>
                 <img src={logo} alt="facebook icon" className="LoginLogoImageCenter"/>
-                <p className = "Login-Title">FacebookLite</p>
-                <form action = "Apps" method = "POST" className="LoginForm">
+                <p className = "Login-Title">FaceBookLite</p>
+                <form onSubmit={this.handleLogin} className="LoginForm">
                     Username:<br/>
-                    <input type="text" className = "Login-Username" name="username" value=""/>
+                    <TextField value={this.state.username} onChange={(e)=>this.handleUsername(e.target.value)} type="text" className = "Login-Username" name="username"/>
                     <br/>
                     Password:<br/>
-                    <input type="text" className="Login-Password" name="password" value=""/>
+                    <TextField value={this.state.password} onChange={(e) =>this.handlePassword(e.target.value)} type="password" className="Login-Password" name="password"/>
                     <br/><br/>
-                    <input type="submit" value="submit"/>
+                    <RaisedButton type="submit" >login</RaisedButton>
                 </form>
                 
 
