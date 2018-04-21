4b1213904d975e57cea311bd5e024bb6b410faef
ArminderK <arminder.khinda@yahoo.com>
2018-04-21 15:47:25 -0700

    Updates the user's dob

--- project4/src/components/Settings/Setting.js
+++ project4/src/components/Settings/Setting.js
@@ -3,6 +3,8 @@ import './Setting.css'
 import Settings from 'material-ui/svg-icons/action/settings';
 import IconButton from 'material-ui/IconButton';
 import Toggle from 'material-ui/Toggle';
+import DatePicker from 'material-ui/DatePicker';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class Setting extends Component {
 
@@ -14,13 +16,30 @@ class Setting extends Component {
         this.setState({showSettings:!this.state.showSettings})
     }
 
+    updateDOB(dob) {
+        dob.preventDefault();
+        let userID = this.props.userID;
+
+        let d = {
+            dob: this.refs.updatedob.refs.input.props.value
+        };
+        
+        fetch(`/updateDOB/${userID}`, {
+            method: 'POST',
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+            body: JSON.stringify(d)
+        }).then(res => res.json()).then(res => this.props.updateDOB(res))
+
+        // this.refs.updatedob.refs.input.props.value = "";
+    }
+
     render() {
         const styles = {
             block: {
                 maxWidth: 100
             },
             toggle: {
-                marginBottom: 5
+                marginBottom: 0
             },
               largeIcon: {
                 width: 60,
@@ -36,12 +55,19 @@ class Setting extends Component {
            
             <div className="settingsSection">
                 {!this.state.showSettings ? '':
-                <div className="toggles">
-                    <Toggle onToggle={this.props.toggleFriends} style={styles.toggle} toggled={this.props.friends?true:false} label="Show Friends"/>
-                    <Toggle onToggle={this.props.togglePosts} toggled={this.props.posts?true:false}  style={styles.toggle} label="Show Posts" defaultToggled={true}/>
-                    <Toggle onToggle={this.props.toggleStatus} toggled={this.props.status?true:false} style={styles.toggle} label="Show Status"/>
-                    <Toggle onToggle={this.props.toggleDOB} toggled={this.props.dob?true:false} style={styles.toggle} label="Show DOB"/>
-                </div>}
+                <div>
+                    <div className="updateDOB">
+                        <DatePicker hintText="Update DOB" textFieldStyle={{width: '135px'}} ref="updatedob" />
+                        <RaisedButton label="Update" secondary={true} onClick={this.updateDOB.bind(this)} />
+                    </div>
+                    <div className="toggles">
+                        <Toggle onToggle={this.props.toggleFriends} style={styles.toggle} toggled={this.props.friends?true:false} label="Show Friends"/>
+                        <Toggle onToggle={this.props.togglePosts} toggled={this.props.posts?true:false}  style={styles.toggle} label="Show Posts" defaultToggled={true}/>
+                        <Toggle onToggle={this.props.toggleStatus} toggled={this.props.status?true:false} style={styles.toggle} label="Show Status"/>
+                        <Toggle onToggle={this.props.toggleDOB} toggled={this.props.dob?true:false} style={styles.toggle} label="Show DOB"/>
+                    </div>
+                </div>
+                }
                 <div className="settingsWrapper">
                     <IconButton onClick={this.toggleSettings} iconStyle={styles.largeIcon} style={styles.large} className="gearIcon">
                         <Settings />
